62ee08f08c76ca0d81dcf2e05113fde205e44dc4
scalewide <scalewide@naver.com>
2022-05-04 20:32:02 +0900

    Add logOut button Profile.js

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,14 @@
-const Profile = () => <span>Profile</span>
+import { authService } from "fbase"
+
+const Profile = () => {
+
+    const onLogOutClick = () => authService.signOut();
+
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log out</button>
+        </>
+    );
+};
 
 export default Profile
