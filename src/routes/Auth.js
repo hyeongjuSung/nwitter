3b14b4a0fb1c1304a5b666f14199b6f6b0ccf9f1
scalewide <scalewide@naver.com>
2022-05-04 19:39:10 +0900

    Add popup Auth.js

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -37,7 +37,7 @@ const Auth = () => {
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
 
-    const onSocialClick = (event) => {
+    const onSocialClick = async (event) => {
         const {
             target: {name},
         } = event;
@@ -47,6 +47,8 @@ const Auth = () => {
         } else if (name === "github") {
             provider = new firebaseInstance.auth.GithubAuthProvider();
         }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
     };
 
     return (
