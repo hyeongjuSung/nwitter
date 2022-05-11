1e41c45498bd248188a49d121023fe00c4b2ac91
scalewide <scalewide@naver.com>
2022-05-11 20:23:26 +0900

    Read doc Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,17 @@
 import { dbService } from "fbase";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        console.log(dbNweets);
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     const onSubmit = async (event) => {
         event.preventDefault();
