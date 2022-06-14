989784299ebe2cbfe66f7d8a0f66ca1aa0fc5964
scalewide <scalewide@naver.com>
2022-06-14 19:42:29 +0900

    Add Profile Update func

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,33 @@
 import { authService, dbService } from "fbase"
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.newDisplayName);
 
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
 
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName}/>
+                <input type="submit" placeholder="Update Profile"/>
+            </form>
             <button onClick={onLogOutClick}>Log out</button>
         </>
     );
